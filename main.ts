6fbf1a7927c0efa8717028c892e19a132a404598
Emirocksr <122539529+Emirocksr@users.noreply.github.com>
2023-01-13 08:59:25 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.StickFigure)
 })
+input.onSound(DetectedSound.Loud, function () {
+    music.playMelody("A - - C C C D F ", 126)
+})
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Skull)
+})
+input.onGesture(Gesture.Shake, function () {
+    music.playMelody("C5 B A G F E D C ", 120)
 })
 basic.showIcon(IconNames.Yes)
